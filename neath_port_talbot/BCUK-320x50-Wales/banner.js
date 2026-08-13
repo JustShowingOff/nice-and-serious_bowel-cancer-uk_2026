ec4321316593249571b375f8848a799f8e302624
Stephen Sargent <steve@justshowingoff.com>
2026-08-13 18:46:45 +0100

    chore: 320x50 Welsh amends

--- neath_port_talbot/BCUK-320x50-Wales/banner.js
+++ neath_port_talbot/BCUK-320x50-Wales/banner.js
@@ -3,7 +3,7 @@
 var loops = 3;
 var currentLoop = 1;
 var container, content, bgExit, loading, timeline, animationTimeline;
-var  bubble1, logos, cover1, cover2, cover3, copy1, copy2, cta
+var  bubble1, logos, cover1, cover2, cover3, copy1,copy1a, copy2, copy2a, cta
 
 
 gsap.registerPlugin(SplitText);
@@ -21,14 +21,16 @@ var init = function () {
     cover2 = document.getElementById('cover2');
      cover3 = document.getElementById('cover3');
     copy1 = document.getElementById('copy1');
-     copy2 = document.getElementById('copy2');
+    copy1a = document.getElementById('copy1a');
+    copy2 = document.getElementById('copy2');
+    copy2a = document.getElementById('copy2a');
     cta = document.getElementById('cta');
     // 
 
 
     // 
     timeline = gsap.timeline({
-        id: "timeline", repeat: 1, delay: 0.25, repeatDelay: 3, onRepeat: function () {
+        id: "timeline", repeat: 0, delay: 0.25, repeatDelay: 3, onRepeat: function () {
             //animationTimeline.play(0)
         }, onStart: function () {
             // animationTimeline.play(0)
@@ -49,11 +51,17 @@ var createAnimation = function () {
 
     var copy1Split = new SplitText(copy1, { type: "words" })
 
+    var copy1aSplit = new SplitText(copy1a, { type: "words" })
+
     var copy2Split = new SplitText(copy2, { type: "lines" })
+
+    var copy2aSplit = new SplitText(copy2a, { type: "lines" })
+
+
 
     gsap.set([bubble1, bubble2], { scale: .5, x: 7, y: -37, transformOrigin: "0% 100%" })
 
-
+    gsap.set([copy1a, copy2a], { display: "none" })
 
     gsap.set(logos, { transformOrigin: "66% 50%" })
 
@@ -79,12 +87,25 @@ var createAnimation = function () {
 
         .from(copy1Split.words, { duration: .5, y: "+=50", alpha: 0, ease: "power1.out", stagger: { amount: 0.35 } }, "frame2+=0.75")
 
+        .to(copy1,{ duration: 0.1, autoAlpha: 0, ease: "none", display: "none" }, "frame2+=3.25")
 
-          .addLabel("frame3", "+=2")
+        .set(copy1a, { display: "block"})
 
-         .from(cover2, { duration: 1, x: "+=320", ease: "power1.inOut" }, "frame3")
+        .from(copy1aSplit.words, { duration: .5, y: "+=50", alpha: 0, ease: "power1.out", stagger: { amount: 0.35 } })
 
-         .from(copy2Split.lines, { duration: .5, y: "+=50", alpha: 0, ease: "power1.out", stagger: { amount: 0.35 } }, "frame3+=1")
+
+
+        .addLabel("frame3", "+=1.5")
+
+        .from(cover2, { duration: 1, x: "+=320", ease: "power1.inOut" }, "frame3")
+
+        .from(copy2Split.lines, { duration: .5, y: "+=50", alpha: 0, ease: "power1.out", stagger: { amount: 0.35 } }, "frame3+=1")
+
+        .to(copy2,{ duration: 0.1, autoAlpha: 0, ease: "none", display: "none" }, "frame3+=3.75")
+
+        .set(copy2a, { display: "block"})
+
+        .from(copy2aSplit.lines, { duration: .5, y: "+=50", alpha: 0, ease: "power1.out", stagger: { amount: 0.35 } })
 
 
 
